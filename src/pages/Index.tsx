b066c6ca41633b32f5836ce5b543a04370d44866
Qasim Bhutta <4qasim@gmail.com>
2025-05-20 17:22:52 -0500

    form changed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import HeroSection from '@/components/InfoSection';
 import CountdownTimer from '@/components/CountdownTimer';
 import SignupForm from '@/components/SignupForm';
+import { Button } from '@/components/ui/button';
 
 const Index = () => {
   // Set the target date for the countdown (30 days from now)
@@ -29,7 +30,18 @@ const Index = () => {
         <div className="flex flex-col items-center justify-center h-[calc(100vh-150px)] mt-5">
           {/* Hero Section */}
           <HeroSection />
-          <SignupForm/>
+          {/* <SignupForm/> */}
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLScNKrbLV_Rx3XxDUCW2H_mgvLrTz83X_97CJxoB5tSEo7P9bw/viewform?usp=dialog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              className="px-20 py-6 text-lg font-bold rounded shadow transition-all duration-200 min-w-[140px] border-2 border-hackathon-accent bg-transparent text-hackathon-accent hover:bg-hackathon-accent hover:text-hackathon-dark"
+            >
+              Register
+            </Button>
+          </a>
           {/* Countdown Timer - Moved slightly higher */}
           <div className="w-full max-w-3xl mt-8">
             <CountdownTimer targetDate={registrationDate} />
